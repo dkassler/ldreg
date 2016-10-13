38d1197797a5a29bbe549ba6f9377ae244ac1904
DKassler <dkassler@mathematica-mpr.com>
2016-10-13 19:36:45 -0400

    Added pacman to description, and completed test code for null/non-null case.

--- Code/test_non_null.R
+++ Code/test_non_null.R
@@ -1,20 +1,73 @@
-library(ldreg)
-library(doParallel)
+# For this process, you need to allocate at least 800 MB of RAM per worker.
 
-x1s <- pblapply(1:1000, function(i) {
+library(ldreg)
+pacman::p_load(doSNOW, progress)
+
+N_cores <- 4
+size <- 100
+
+cl <- makeCluster(N_cores)
+registerDoSNOW(cl)
+
+pb <- progress_bar(1000)
+x1s <- foreach(
+  i = 1:1000,
+  .options.snow = list(progress = function(n) pb$tick())
+) %dopar% {
   set.seed(i)
-  rand_sim_data(N_snp = 10000, N1 = 10000, N_refpop = 10000, Ns = 3000)
-})
+  rand_sim_data(N_snp = size, N1 = size, N_refpop = size, Ns = round(0.3 * size))
+}
 
 tus <- sapply(x1s, function(.) sapply(.[["cat_mats"]], `[`, 1, 2))
 sel1 <- which.max(colSums(abs(tus)[-1,]))
 x1 <- x1s[[sel1]]
 
-x2s <- pbreplicate(100, simplify = FALSE, do.call(sim1, x1))
-jack <- lapply(x2s, function(x2) do.call(jackknife, c(x2, x1)))
-jack_wt <- lapply(x2s, function(x2) do.call(jackknife, c(x2, x1, list(weighted = TRUE))))
-fits <- sapply(jack, `[[`, "upsilon_hat")
-fits_wt <- sapply(jack_wt, `[[`, "upsilon_hat")
+# x2s <- pbreplicate(100, simplify = FALSE, do.call(sim1, x1))
+# jack <- lapply(x2s, function(x2) do.call(jackknife, c(x2, x1)))
+# jack_wt <- lapply(x2s, function(x2) do.call(jackknife, c(x2, x1, list(weighted = TRUE))))
+# fits <- sapply(jack, `[[`, "upsilon_hat")
+# fits_wt <- sapply(jack_wt, `[[`, "upsilon_hat")
+
+pb <- progress_bar(100)
+jack_res <- foreach(
+  1:100,
+  .options.snow = list(progress = function(n) pb$tick())
+) %dopar% {
+  x2 <- do.call(sim1, x1)
+  list(
+    jack = do.call(jackknife, c(x2, x1)),
+    jack_wt = do.call(jackknife, c(x2, x1, list(weighted = TRUE)))
+  )
+}
+
+stopCluster(cl)
+
+saveRDS(jack_res, "jack_raw_nonnull_10K.rds")
+
+rm(list = ls())
+
+cl <- makeCluster(N_cores)
+registerDoSNOW(cl)
+
+x1 <- rand_sim_data(N_snp = size, N1 = size, N_refpop = size, Ns = round(0.3 * size),
+                    cat_props = c(1, 0.4))
+x1$cat_mats <- matrix(c(0.8, 0, 0, 0.5), nrow = 2)
+
+pb <- progress_bar(100)
+jack_res <- foreach(
+  1:100,
+  .options.snow = list(progress = function(n) pb$tick())
+) %dopar% {
+  x2 <- do.call(sim1, x1)
+  list(
+    jack = do.call(jackknife, c(x2, x1)),
+    jack_wt = do.call(jackknife, c(x2, x1, list(weighted = TRUE)))
+  )
+}
+
+stopCluster(cl)
+
+saveRDS(jack_res, "jack_raw_null_10K.rds")
 
 # tmp_df_1 <- t(fits) %>%
 #   as.data.frame() %>%
