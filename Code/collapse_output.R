bfdc03e6798388e002dfdc7614e03758b2d3253a
DKassler <dkassler@mathematica-mpr.com>
2016-11-01 22:47:50 -0400

    made some updates to collapse function

--- Code/collapse_output.R
+++ Code/collapse_output.R
@@ -6,12 +6,19 @@ library(foreach)
 if (length(script_args) < 1) stop("Script run without any arguments")
 path_arg <- script_args[[1]]
 
+print(path_arg)
+
 dir_filenames <- dir(dirname(path_arg))
 filenames <- grep(glob2rx(basename(path_arg)), dir_filenames, value = T)
 
-out <- foreach(name = filenames) %do% {
+if (length(filenames) == 0) stop("No matching files found")
+print(filenames)
+
+out <- foreach(name = filenames,
+               .errorhandling = "pass") %do% {
   filename <- file.path(dirname(path_arg), name)
   out <- readRDS(filename)
+  print(sprintf("read %s", filename))
   file.remove(filename)
   out
 }
