66d7a411300edee0abff0d36155edad367db83ea
DKassler <dkassler@mathematica-mpr.com>
2016-11-30 23:36:32 -0500

    Further updates to both processing memo and pass_true_r_non_null (sticking with fixed known seed for now, for better comparability, but may adjust this in the future)

--- Code/sim_testing/pass_true_r_non_null.R
+++ Code/sim_testing/pass_true_r_non_null.R
@@ -6,10 +6,20 @@ jack_args <- list(
   bias_correction = TRUE
 )
 
-saveLSF(.Random.seed, "seed")
-
+x1_seed <- if (size == 100) {
+  938 #/1000
+} else if (size == 10000) {
+  180 #/1000
+} else NULL
+set.seed(x1_seed)
 x1 <- rand_sim_data(N_snp = size, N1 = size, N_refpop = size,
                     Ns = round(0.3 * size))
+set.seed(NULL)
+
+saveLSF(.Random.seed, "seed")
+
+# x1 <- rand_sim_data(N_snp = size, N1 = size, N_refpop = size,
+#                     Ns = round(0.3 * size))
 x2 <- do.call(sim1, x1)
 jack_emp <- do.call(jackknife, c(x2, x1, jack_args))
 jack_wt_emp <- do.call(jackknife, c(x2, x1, jack_args, weighted = TRUE))
