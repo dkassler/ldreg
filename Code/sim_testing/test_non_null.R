6673149bc4f99d13dea0ba3d57f86f0d46fc4591
Daniel Kassler <dkassler@uchicago.edu>
2016-12-11 21:32:14 -0500

    made updates to test_non_null

--- Code/sim_testing/test_non_null.R
+++ Code/sim_testing/test_non_null.R
@@ -3,7 +3,7 @@ library(ldreg)
 size <- 10000
 jack_args <- list(
   blocks = 200,
-  find_blocks = FALSE
+  find_blocks = TRUE
 )
 
 x1_seed <- if (size == 100) {
@@ -16,9 +16,8 @@ x1 <- rand_sim_data(N_snp = size, N1 = size, N_refpop = size,
                     Ns = round(0.3 * size))
 set.seed(NULL)
 
-#saveLSF(.Random.seed, "seed")
-saveLSF_rng("seed")
-#restore_rng(sprintf("output/test_non_null/2016-12-11/out/seed.%s.rds", getopts()$jobindex))
+#saveLSF_rng("seed")
+restore_rng(sprintf("output/test_non_null/2016-12-11b/out/seed.%s", getopts()$jobindex))
 
 x2 <- do.call(sim1, x1)
 #jack <- do.call(jackknife, c(x2, x1, jack_args))
